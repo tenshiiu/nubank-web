d91257c2711aa438177dd9e9e7fe72e14c927839
Raul Vinícius <165701907+tenshiiu@users.noreply.github.com>
2024-10-22 16:44:45 -0300

    React Icons no Slide 2

--- src/slide/slide2.tsx
+++ src/slide/slide2.tsx
@@ -1,12 +1,18 @@
 import { MdOutlineArrowOutward } from "react-icons/md";
 import styles from "./slide2.module.css";
 
+import { GiReceiveMoney } from "react-icons/gi";
+import { TbPigMoney } from "react-icons/tb";
+import { FaRegAddressCard } from "react-icons/fa";
+import { TbChartInfographic } from "react-icons/tb";
+
 export function Slide2() {
     return (
         <section>
 
             <div className={styles.container}>
                 <div className={styles.card}>
+                    <GiReceiveMoney className={styles.icons}/>
                     
                         <h1>Empréstimo</h1>
                         
@@ -14,13 +20,15 @@ export function Slide2() {
                     <p><a href="#">Saiba mais <MdOutlineArrowOutward /></a></p>
                 </div>
                 <div className={styles.card}>
-                    
+                    <FaRegAddressCard className={styles.icons}/>
+
                         <h1>NuConsignado</h1>
                         
-                    <p>O empréstimo consignado para interesses de servidores públicos federais e para aposentados e pensionistas do INSS.</p>
+                    <p>O empréstimo consignado para interesses de servidores públicos e para aposentados e pensionistas do INSS.</p>
                     <p><a href="#">Saiba mais <MdOutlineArrowOutward /></a></p>
                 </div>
                 <div className={styles.card}>
+                    <TbChartInfographic className={styles.icons}/>
                     
                         <h1>Investimentos</h1>
                         
@@ -28,7 +36,8 @@ export function Slide2() {
                     <p><a href="#">Saiba mais <MdOutlineArrowOutward /></a></p>
                 </div>
                 <div className={styles.card}>
-                    
+                    <TbPigMoney className={styles.icons}/>
+
                         <h1>Caixinhas</h1>
                         
                     <p>Organize e guarde dinheiro para seus planos. Crie Caixinhas personalizadas para cada um de seus objetivos.</p>
